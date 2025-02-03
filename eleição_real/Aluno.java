0e9f73f073e7d043bdab8a13ce012120fe72be5a
Carlos Eduardo <carlos.vitor@escolar.ifrn.edu.br>
2025-02-03 10:22:28 -0300

    Update Aluno.java

--- eleição_real/Aluno.java
+++ eleição_real/Aluno.java
@@ -1,6 +1,25 @@
 package eleição_real;
+import java.time.LocalDate;
 
-public class Aluno {
+public class Aluno extends Pessoa {
     private String curso;
+
+    Aluno(String nome, int cpf, int matricula, LocalDate dataNascimento, String curso) {
+        super(nome, cpf, matricula, dataNascimento);
+        this.curso = curso;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    }
+
+
+
     
 }
