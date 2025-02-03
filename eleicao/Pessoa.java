bd8377a29ad8b0785ad7bc8f670c2a1493149b7f
J0a0p3dr067 <pjpedro158@gmail.com>
2025-02-03 09:42:19 -0300

    metodos da classe pessoa

--- eleicao/Pessoa.java
+++ eleicao/Pessoa.java
@@ -1,6 +1,6 @@
 package eleicao;
 
-import java.time.LocalDate;
+import java.time.LocalDate; 
 
 public abstract class Pessoa {
     private String nome;
--- eleição_real/Pessoa.java
+++ eleição_real/Pessoa.java
@@ -1,9 +1,43 @@
 package eleição_real;
 
+import java.time.LocalDate;
+
 public abstract class Pessoa {
     private String nome;
     private int cpf, matricula;
-    
+    private LocalDate dataNascimento;
 
+
+    public Pessoa(String nome, int cpf, int matricula, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.matricula = matricula;
+        this.dataNascimento = dataNascimento;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public int getIdade() {
+        return LocalDate.now().getYear() - dataNascimento.getYear();
+    }
+
+    
+    
 
 }
