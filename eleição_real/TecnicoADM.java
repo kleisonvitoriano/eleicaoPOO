4e57eb01e5412b2971b2b807b1bc62aa406092b9
J0a0p3dr067 <pjpedro158@gmail.com>
2025-02-18 08:26:27 -0300

    ajeitando tecnicoADM

--- eleição_real/TecnicoADM.java
+++ eleição_real/TecnicoADM.java
@@ -3,13 +3,21 @@ import java.time.LocalDate;
 public class TecnicoADM extends Servidor {
     private int tempoEfetivoServico;
     private LocalDate tempoCargoGestao;
+    private String regimeTrabalho;
 
 
-   public TecnicoADM(String nome, String cpf, int matricula, LocalDate dataNascimento, String campus, Titulacao titulacao, boolean efetivo, int tempoEfetivoServico, LocalDate tempoCargoGestao) {
-        super(nome, cpf, matricula, dataNascimento, campus, titulacao, tempoCargoGestao, efetivo); 
+   
+
+    public TecnicoADM(String nome, String cpf, int matricula, LocalDate dataNascimento, String campus,
+            eleição_real.Titulacao titulacao, LocalDate inicio_carreira, boolean efetivo, String responsavel,
+            int tempoEfetivoServico, LocalDate tempoCargoGestao, String regimeTrabalho) {
+        super(nome, cpf, matricula, dataNascimento, campus, titulacao, inicio_carreira, efetivo, responsavel);
         this.tempoEfetivoServico = tempoEfetivoServico;
         this.tempoCargoGestao = tempoCargoGestao;
-    } 
+        this.regimeTrabalho = regimeTrabalho;
+    }
+
+   
 
     public int getTempoEfetivoServico() {
         return tempoEfetivoServico;
@@ -24,7 +32,23 @@ public class TecnicoADM extends Servidor {
         return tempoEfetivoServico >= 5 && idade >= 35;
     }
 
+    public void setTempoEfetivoServico(int tempoEfetivoServico) {
+        this.tempoEfetivoServico = tempoEfetivoServico;
+    }
 
+    public void setTempoCargoGestao(LocalDate tempoCargoGestao) {
+        this.tempoCargoGestao = tempoCargoGestao;
+    }
+
+    public String getRegimeTrabalho() {
+        return regimeTrabalho;
+    }
+
+    public void setRegimeTrabalho(String regime) {
+        this.regimeTrabalho = regime;
+    }
+
+    
 
 
 
