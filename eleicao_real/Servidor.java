1e4c4f8319541d0989ac581245e238b37d9fa4eb
Carlos Eduardo <carlos.vitor@escolar.ifrn.edu.br>
2025-02-22 10:14:16 -0300

    Atualizar o Servidor.java

--- eleicao_real/Servidor.java
+++ eleicao_real/Servidor.java
@@ -2,90 +2,29 @@ package eleicao_real;
 
 import java.time.LocalDate;
 
-public class Servidor extends Pessoa {
-    private String campus;
-    private Titulacao Titulacao;
-    private LocalDate inicio_carreira;
-    private boolean efetivo;
-    private String responsavel;
-
-    public Servidor(String nome, String cpf, int matricula, LocalDate dataNascimento, String campus,
-            eleicao_real.Titulacao titulacao, LocalDate inicio_carreira, boolean efetivo, String responsavel) {
-        super(nome, cpf, matricula, dataNascimento);
-        this.setCampus(campus);
-        this.setTitulacao(titulacao);
-        this.setinicio_carreira(inicio_carreira);
-        this.setEfetivo(efetivo);
-        this.setResponsavel(responsavel);
-    }
-
-    public String getResponsavel() {
-        return responsavel;
-    }
-
-    public void setResponsavel(String responsavel) {
+public abstract class Servidor extends Pessoa {
+    protected String campus;
+    protected Titulacao titulacao;
+    protected boolean efetivo;
     
-        this.responsavel = responsavel;
-    }
-    public Servidor(String nome, String cpf, int matricula, LocalDate dataNascimento, String campus,
-            eleicao_real.Titulacao titulacao, LocalDate inicio_carreira, boolean efetivo) {
-        super(nome, cpf, matricula, dataNascimento);
-        this.setCampus(campus);
-        this.setTitulacao(titulacao);
-        this.setinicio_carreira(inicio_carreira);
-        this.setEfetivo(efetivo);
+    public Servidor(String nome, String cpf, LocalDate dataNascimento, 
+                   String matricula, String campus, Titulacao titulacao, 
+                   boolean efetivo) {
+        super(nome, cpf, dataNascimento, matricula);
+        this.campus = campus;
+        this.titulacao = titulacao;
+        this.efetivo = efetivo;
     }
 
-
+    public boolean isEfetivo() {
+        return efetivo;
+    }
 
     public String getCampus() {
         return campus;
     }
 
-
-
-    public void setCampus(String campus) {
-        this.campus = campus;
-    }
-
-
-
     public Titulacao getTitulacao() {
-        return Titulacao;
+        return titulacao;
     }
-
-
-
-    public void setTitulacao(Titulacao titulacao) {
-        Titulacao = titulacao;
-    }
-
-
-
-    public boolean getEfetivo() {
-        return efetivo;
-
-    }
-
-
-
-    public void setEfetivo(boolean efetivo) {
-        this.efetivo = efetivo;
-    }
-
-
-
-    public int getinicio_carreira() {
-        return LocalDate.now().getYear() - inicio_carreira.getYear();
-    }
-
-
-
-    public void setinicio_carreira(LocalDate inicio_carreira) {
-        this.inicio_carreira = inicio_carreira;
-    }
-
-    
-    
-    
-}
+} 
