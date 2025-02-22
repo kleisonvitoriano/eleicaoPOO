f89f00e3ef0d7db8a01ab46392d9e950479eabe5
Carlos Eduardo <carlos.vitor@escolar.ifrn.edu.br>
2025-02-22 10:06:24 -0300

    Atualizar o Aluno.java

--- eleicao_real/Aluno.java
+++ eleicao_real/Aluno.java
@@ -1,26 +1,32 @@
 package eleicao_real;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Aluno extends Pessoa {
     private String curso;
+    private static List<Aluno> alunos = new ArrayList<>();
 
-    
-    
-   public Aluno(String nome, String cpf, int matricula, LocalDate dataNascimento, String curso) {
-        super(nome, cpf, matricula, dataNascimento);
-        this.setCurso(curso);;
+    public Aluno(String nome, String cpf, LocalDate dataNascimento,
+                 String matricula, String curso) {
+        super(nome, cpf, dataNascimento, matricula);
+        this.curso = curso;
+        alunos.add(this);
     }
 
     public String getCurso() {
         return curso;
     }
-
-    public void setCurso(String curso) {
-        this.curso = curso;
+    
+    public static int getTotalAlunos() {
+        return alunos.size();
     }
-
+    
+    public static List<Aluno> getAlunos() {
+        return new ArrayList<>(alunos);
     }
-
+} 
 
 
     
